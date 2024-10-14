abf814083075532d5b75da63cb20eb3354adeab4
tkarabelas <thomas.karabelas.20@cnu.edu>
2024-10-14 18:56:04 -0400

    New_entry
    Added new entry to describe about Topics for Users

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 def index(request):
     return render(request, 'learning_logs/index.html')
@@ -31,3 +31,23 @@ def new_topic(request):
         
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Add a new entry for a topic"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.meth != 'POST':
+        # no data submitted; create a blank form
+        form = EntryForm()
+    else:
+        # POST data submiteed; create a blank form
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
